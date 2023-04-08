36f78490c6ab4e892483bdb47c91f544b612e499
Dinesh Das <dineshdas1016@gmail.com>
2023-04-08 17:58:10 +0530

    Till Pipes

---
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent {
+
+  hotelName :string ="Grand Hayatt";
+
+  numberOfRooms = 10;
+
+  hideRooms = false;
+  rooms: Room ={
+    availableRooms:10,
+    bookedRooms:5,
+    totalRooms: 20
+  };
+  roomList :RoomList[] =[
+    {
+      roomNumber:1,
+      roomType:"Deluxe Room",
+      amenities: "Air conditioner, Free WIFI, TV, Bathroom, Kitchen",
+      price: 500,
+      photos:"https://unsplash.com/photos/_DaoqR8akYk",
+      checkinTime: new Date('11-Nov-2022'),
+      checkoutTime: new Date('12-Nov-2022'),
+      rating:4.22345
+    },
+    {
+      roomNumber:3,
+      roomType:"Private Suite",
+      amenities: "Air conditioner, Free WIFI, TV, Bathroom, Kitchen",
+      price: 15000,
+      photos:"https://unsplash.com/photos/_DaoqR8akYk",
+      checkinTime: new Date('11-Nov-2022'),
+      checkoutTime: new Date('12-Nov-2022'),
+      rating:4.5678
+    },
+    {
+      roomNumber:2,
+      roomType:"Deluxe Room",
+      amenities: "Air conditioner, Free WIFI, TV, Bathroom, Kitchen",
+      price: 1000,
+      photos:"https://unsplash.com/photos/_DaoqR8akYk",
+      checkinTime: new Date('11-Nov-2022'),
+      checkoutTime: new Date('12-Nov-2022'),
+      rating:2.6732
+    }
+  ];
+
+  constructor(){}
+
+  toggle()
+  {
+    this.hideRooms=!this.hideRooms;
+  }
+
+}
