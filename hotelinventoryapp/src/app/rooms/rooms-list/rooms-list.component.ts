c3b10c45866f1e90f48f9819ca29f79f600d3d73
Dinesh Das <dineshdas1016@gmail.com>
2023-04-10 21:58:08 +0530

    Completed ngOnChanges

--- hotelinventoryapp/src/app/rooms/rooms-list/rooms-list.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { RoomList } from '../rooms';
 
 @Component({
   selector: 'app-rooms-list',
   templateUrl: './rooms-list.component.html',
-  styleUrls: ['./rooms-list.component.scss']
+  styleUrls: ['./rooms-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RoomsListComponent {
+export class RoomsListComponent implements OnChanges {
+
+  @Input() rooms: RoomList[] = [];
+  @Input() title: string = '';
+
+  @Output() selectedRoom = new EventEmitter<RoomList>();
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom.emit(room);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // throw new Error('Method not implemented.');
+    console.log(changes);{}
+    if(changes['title']){
+      this.title = changes['title'].currentValue.toUpperCase();
+    }
+  }
+
 
 }
--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -13,6 +13,10 @@ export class RoomsComponent implements OnInit {
   numberOfRooms = 10;
 
   hideRooms = false;
+
+  title= "Room List";
+
+  selectedRoom!: RoomList;
   rooms: Room = {
     availableRooms: 10,
     bookedRooms: 5,
@@ -60,6 +64,27 @@ export class RoomsComponent implements OnInit {
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+    this.title = "Rooms List";
+  }
+
+  selectRoom(room: RoomList) {
+    console.log(room);
+    this.selectedRoom = room;
+  }
+
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 4,
+      roomType: "Deluxe Room",
+      rating: 4.567,
+      amenities: 'Air conditioner, Free wifi, tv, bathroom, kitchen',
+      price: 500,
+      photos: 'https://unsplash.com/photos/_DaoqR8akYk',
+      checkinTime: new Date('11-Nov-2022'),
+      checkoutTime: new Date('12-Nov-2022')
+    }
+    // this.roomList.push(room);
+    this.roomList = [...this.roomList,room];
   }
 
 }
