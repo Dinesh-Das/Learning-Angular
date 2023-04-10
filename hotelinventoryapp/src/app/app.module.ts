b3c6899fe3fa1c80e5b3a0bd233de867f9463cb7
Dinesh Das <dineshdas1016@gmail.com>
2023-04-10 19:25:24 +0530

    Bootstrap demo

--- hotelinventoryapp/src/app/app.module.ts
+++ hotelinventoryapp/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RoomsListComponent } from './rooms/rooms-list/rooms-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoomsComponent
+    RoomsComponent,
+    RoomsListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Room, RoomList } from './rooms';
 
 @Component({
@@ -6,56 +6,60 @@ import { Room, RoomList } from './rooms';
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit {
 
-  hotelName :string ="Grand Hayatt";
+  hotelName: string = "Grand Hayatt";
 
   numberOfRooms = 10;
 
   hideRooms = false;
-  rooms: Room ={
-    availableRooms:10,
-    bookedRooms:5,
+  rooms: Room = {
+    availableRooms: 10,
+    bookedRooms: 5,
     totalRooms: 20
   };
-  roomList :RoomList[] =[
-    {
-      roomNumber:1,
-      roomType:"Deluxe Room",
-      amenities: "Air conditioner, Free WIFI, TV, Bathroom, Kitchen",
-      price: 500,
-      photos:"https://unsplash.com/photos/_DaoqR8akYk",
-      checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('12-Nov-2022'),
-      rating:4.22345
-    },
-    {
-      roomNumber:3,
-      roomType:"Private Suite",
-      amenities: "Air conditioner, Free WIFI, TV, Bathroom, Kitchen",
-      price: 15000,
-      photos:"https://unsplash.com/photos/_DaoqR8akYk",
-      checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('12-Nov-2022'),
-      rating:4.5678
-    },
-    {
-      roomNumber:2,
-      roomType:"Deluxe Room",
-      amenities: "Air conditioner, Free WIFI, TV, Bathroom, Kitchen",
-      price: 1000,
-      photos:"https://unsplash.com/photos/_DaoqR8akYk",
-      checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('12-Nov-2022'),
-      rating:2.6732
-    }
-  ];
+  roomList: RoomList[] = [];
 
-  constructor(){}
+  constructor() { }
 
-  toggle()
-  {
-    this.hideRooms=!this.hideRooms;
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: "Deluxe Room",
+        amenities: "Air conditioner, Free WIFI, TV, Bathroom, Kitchen",
+        price: 500,
+        photos: "https://unsplash.com/photos/_DaoqR8akYk",
+        checkinTime: new Date('11-Nov-2022'),
+        checkoutTime: new Date('12-Nov-2022'),
+        rating: 4.22345
+      },
+      {
+        roomNumber: 3,
+        roomType: "Private Suite",
+        amenities: "Air conditioner, Free WIFI, TV, Bathroom, Kitchen",
+        price: 15000,
+        photos: "https://unsplash.com/photos/_DaoqR8akYk",
+        checkinTime: new Date('11-Nov-2022'),
+        checkoutTime: new Date('12-Nov-2022'),
+        rating: 4.5678
+      },
+      {
+        roomNumber: 2,
+        roomType: "Deluxe Room",
+        amenities: "Air conditioner, Free WIFI, TV, Bathroom, Kitchen",
+        price: 1000,
+        photos: "https://unsplash.com/photos/_DaoqR8akYk",
+        checkinTime: new Date('11-Nov-2022'),
+        checkoutTime: new Date('12-Nov-2022'),
+        rating: 2.6732
+      }
+    ];
+
+  }
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
   }
 
 }
